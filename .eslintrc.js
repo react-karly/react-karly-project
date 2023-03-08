75e7d89d6a1500cce7a809702c545c1ded9b7b73
cf9874 <cf9874@gmail.com>
2023-03-08 23:37:18 +0900

    Prettier Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,87 +6,87 @@ module.exports = {
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "import", "react-hooks"],
+  plugins: ['react', 'import', 'react-hooks', 'prettier'],
   rules: {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "no-console": "warn",
-    "react/prop-types": "off",
-    "react/button-has-type": "warn",
-    "react/self-closing-comp": [
-      "warn",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-console': 'warn',
+    'react/prop-types': 'off',
+    'react/button-has-type': 'warn',
+    'react/self-closing-comp': [
+      'warn',
       {
         component: true,
         html: false,
       },
     ],
-    "react/jsx-sort-props": [
-      "warn",
+    'react/jsx-sort-props': [
+      'warn',
       {
         shorthandFirst: true,
         callbacksLast: true,
         noSortAlphabetically: false,
         reservedFirst: true,
-        multiline: "last",
+        multiline: 'last',
       },
     ],
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
         groups: [
-          "builtin",
-          "external",
-          "internal",
-          ["sibling", "parent", "index"],
-          "type",
-          "unknown",
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent', 'index'],
+          'type',
+          'unknown',
         ],
         pathGroups: [
           {
-            pattern: "{react*,react*/**}",
-            group: "external",
-            position: "before",
+            pattern: '{react*,react*/**}',
+            group: 'external',
+            position: 'before',
           },
           {
-            pattern: "{./**/*.module.css,./**/*.css}",
-            group: "unknown",
+            pattern: '{./**/*.module.css,./**/*.css}',
+            group: 'unknown',
           },
         ],
-        pathGroupsExcludedImportTypes: ["react", "unknown"],
-        "newlines-between": "always-and-inside-groups",
+        pathGroupsExcludedImportTypes: ['react', 'unknown'],
+        'newlines-between': 'always-and-inside-groups',
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
       },
     ],
-    "jsx-a11y/anchor-has-content": [
-      "warn",
+    'jsx-a11y/anchor-has-content': [
+      'warn',
       {
-        components: ["Link"],
+        components: ['Link'],
       },
     ],
-    "jsx-a11y/anchor-is-valid": [
-      "warn",
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
       {
-        components: ["Link"],
+        components: ['Link'],
       },
     ],
   },
