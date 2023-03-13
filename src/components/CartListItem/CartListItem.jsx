3336994e47f4709424b19c9d0207efc9d065dd42
somin <smhs12@naver.com>
2023-03-13 14:56:20 +0900

    [chore] 파일 export 방법 수정 및 스타일 import 네임 수정

--- src/components/CartListItem/CartListItem.jsx
+++ src/components/CartListItem/CartListItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Counter from '../Counter/Counter';
+import { Counter } from '../Counter/Counter';
 import close from '@/assets/cart/Cancel.png';
-import cartItem from './CartListItem.module.css';
+import styles from './CartListItem.module.css';
 import productImg from '@/assets/tangtang/thumbnail.jpg';
 import unChecked from '@/assets/cart/isChecked=false.png';
 function CartListItem() {
   return (
-    <li className={cartItem['cart-item-container']}>
-      <button type="button" className={cartItem['check-button']}>
+    <li className={styles['cart-item-container']}>
+      <button type="button" className={styles['check-button']}>
         <img src={unChecked} alt="상품선택" width="24" height="24" />
       </button>
       <img
@@ -15,18 +15,16 @@ function CartListItem() {
         alt=""
         width="60"
         height="78"
-        className={cartItem['product-image']}
+        className={styles['product-image']}
       />
-      <h4 className={cartItem.name}>
-        &#91;풀무원&#93; 탱탱쫄면 &#40;4개입&#41;
-      </h4>
+      <h4 className={styles.name}>&#91;풀무원&#93; 탱탱쫄면 &#40;4개입&#41;</h4>
       <Counter />
-      <span className={cartItem.price}>4,980원</span>
-      <button type="button" className={cartItem.button}>
+      <span className={styles.price}>4,980원</span>
+      <button type="button" className={styles.button}>
         <img src={close} alt="닫기" width="30" height="30" />
       </button>
     </li>
   );
 }
 
-export default CartListItem;
+export { CartListItem };
