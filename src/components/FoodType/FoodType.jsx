b52b5b951d3b3c27f967ddbc282f77e51dea6e93
somin <smhs12@naver.com>
2023-03-13 16:50:11 +0900

    [feat] 상품 상세 리스트 토글 버튼 기능 추가 #97

--- src/components/FoodType/FoodType.jsx
+++ src/components/FoodType/FoodType.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FoodType.module.css';
 import frozenImg from '@/assets/cart/Type=Frozen.png';
 import normalImg from '@/assets/cart/Type=Normal.png';
+import directionUp from '@/assets/cart/Direction=Up.png';
 import directionDown from '@/assets/cart/Direction=Down.png';
 import refrigeratedImg from '@/assets/cart/Type=Refrigerated.png';
 import { CartListItem } from '../CartListItem/CartListItem';
 
 function FoodType({ type }) {
+  const [isShowList, setIsShowList] = useState(false);
+  const handleShowList = () => {
+    setIsShowList((isShowList) => !isShowList);
+  };
   return (
     <div className={styles['food-type-container']}>
       <div type="button" className={styles.wrapper}>
@@ -14,17 +19,15 @@ function FoodType({ type }) {
         <h3 className={styles.title}>{type.title}</h3>
         <button type="button" className={styles.button}>
           <img
-            src={directionDown}
+            src={isShowList ? directionUp : directionDown}
             alt="상품 리스트 열기"
             width="18"
             height="18"
+            onClick={handleShowList}
           />
         </button>
       </div>
-      <ul className={styles.list}>
-        <CartListItem />
-        <CartListItem />
-      </ul>
+      <ul className={styles.list}>{isShowList && <CartListItem />}</ul>
     </div>
   );
 }
