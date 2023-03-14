bbe03911b4af3ddfbf6e76516047383c609d85a4
yurimpark <yuriming112@gmail.com>
2023-03-14 10:44:19 +0900

    [style] 가입하기 버튼 추가 및 일부 마크업 수정

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -35,35 +35,44 @@ const Register = () => {
             <BirthdayInput />
             <EventRadioBtn />
           </ul>
+          <ul className="register-terms">
+            <li>
+              <p>
+                <span className={app['a11y-hidden']}>필수 선택사항</span>
+                이용약관 동의
+              </p>
+            </li>
+            <li>
+              <ul>
+                <li>
+                  <span className={app['a11y-hidden']}>
+                    이용약관 전체 동의 여부
+                  </span>
+                  <p>
+                    선택 항목에 동의하지 않는 경우도 회원가입 및 일반적인
+                    서비스를 이용할 수 있습니다.
+                  </p>
+                  <input type="checkbox" id="check-all-terms" />
+                  <label htmlFor="check-all-terms">전체 동의합니다.</label>
+                </li>
+                <RegisterTOS terms="이용약관 동의" requiredOrOptional="필수" />
+                <RegisterTOS
+                  terms="개인정보 수집 · 이용 동의"
+                  requiredOrOptional="필수"
+                />
+                <RegisterTOS
+                  terms="무료배송, 할인쿠폰 등 혜택/정보 수신 동의"
+                  requiredOrOptional="선택"
+                />
+                <RegisterTOS
+                  terms="본인은 만 14세 이상입니다."
+                  requiredOrOptional="필수"
+                />
+              </ul>
+            </li>
+          </ul>
+          <Btn btnTitle="가입하기" />
         </form>
-        <ul>
-          <li>
-            <label>
-              <span className={app['a11y-hidden']}>필수입력사항</span>
-              이용약관 동의
-            </label>
-          </li>
-          <span className={app['a11y-hidden']}>이용약관 전체 동의 선택</span>
-          <input type="checkbox" id="check-all-terms" />
-          <label htmlFor="check-all-terms">전체 동의합니다.</label>
-          <p>
-            선택 항목에 동의하지 않는 경우도 회원가입 및 일반적인 서비스를
-            이용할 수 있습니다.
-          </p>
-          <RegisterTOS terms="이용약관 동의" requiredOrOptional="필수" />
-          <RegisterTOS
-            terms="개인정보 수집 · 이용 동의"
-            requiredOrOptional="필수"
-          />
-          <RegisterTOS
-            terms="무료배송, 할인쿠폰 등 혜택/정보 수신 동의"
-            requiredOrOptional="선택"
-          />
-          <RegisterTOS
-            terms="본인은 만 14세 이상입니다."
-            requiredOrOptional="필수"
-          />
-        </ul>
       </section>
     </main>
   );
