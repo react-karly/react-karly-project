1a802397405a75c89b5a1bf54e86654853df3bbe
yurimpark <yuriming112@gmail.com>
2023-03-15 17:23:14 +0900

    [style] Address 스타일링 추가

--- src/components/Register/Address.jsx
+++ src/components/Register/Address.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import app from '../../App.module.css';
 import Btn from './Btn';
+import input from '../Register/Input.module.css';
+import address from '../Register/Address.module.css';
 
 const Address = () => {
   return (
     <li>
-      <label>
+      <label className={input['register-label']}>
         <span className={app['a11y-hidden']}>필수입력사항</span>
         주소
       </label>
-      <Btn btnTitle="주소 검색" />
-      <p>배송지에 따라 상품 정보가 달라질 수 있습니다.</p>
+      <div className={address['address-wrapper']}>
+        <Btn btnTitle="주소 검색" buttonClassName="big-btn" />
+        <p className={address['address-desc']}>
+          배송지에 따라 상품 정보가 달라질 수 있습니다.
+        </p>
+      </div>
     </li>
   );
 };
