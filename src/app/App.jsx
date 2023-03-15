07235adac3d5dab75fac227fdda86bfe1ac080b8
BrightSton <gusehf321@naver.com>
2023-03-15 15:14:17 +0900

    [feat] Side-Menu 구현 완료
    - [style] mark-up & styling
    - [refactor] component destruct
    - [feat] 체크박스 선택에 따라 체크 갯수 표현

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import './App.module.css';
+import ProductList from '../pages/ProductList/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from './Layout/Layout';
+export * from './ListCard/ListCard';
+export * from './Container/Container';
+export * from './ListSideMenu/ListSideMenu';
