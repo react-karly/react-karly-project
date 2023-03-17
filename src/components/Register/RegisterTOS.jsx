f58aee10f2aa8f7e25dbdfdcc81129360a9be9f9
yurim park <yuriming112@gmail.com>
2023-03-17 17:32:10 +0900

    [style] 마크업 및 CSS 스타일링

--- src/components/Register/RegisterTOS.jsx
+++ src/components/Register/RegisterTOS.jsx
@@ -8,13 +8,7 @@ export function RegisterTOS({ requiredOrOptional, terms }) {
         이용약관 동의 여부 ({requiredOrOptional} 항목)
       </span>
       <button>약관보기</button>
-      <input
-        className="a11y-hidden terms"
-        type="checkbox"
-        id="terms-of-use"
-        name="terms-of-use"
-        required
-      />
+      <input type="checkbox" id="terms-of-use" name="terms-of-use" required />
       <label htmlFor="terms-of-use">
         {terms} ({requiredOrOptional})
       </label>
