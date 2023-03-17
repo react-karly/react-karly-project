967b362ad1eb7b245167cfe04365aa2a8cfd6ecb
cf9874 <cf9874@gmail.com>
2023-03-17 16:26:03 +0900

    [test] 스와이퍼 content data handling

--- src/components/Swiper/SwiperContent.jsx
+++ src/components/Swiper/SwiperContent.jsx
@@ -4,24 +4,39 @@ import cartIcon from '@/assets/icons/Icon/Cart.svg';
 import jjolmyeon from '@/assets/main/product01.png';
 
 export function SwiperContent({ data, isbanner }) {
+  // console.log(data);
   return isbanner === 'true' ? (
     <a href={data.href}>
-      <img src={data.src} alt={data.title} />
+      <img src={data.src} alt={data.title} className={styles['banner-img']} />
     </a>
   ) : (
     <a href="/">
       <img
         className={styles['recommend-products-item__image']}
         src={jjolmyeon}
-        alt="풀무원 탱탱쫄면 4개입"
+        alt={data.image.alt}
       />
       <div className={styles['recommend-products-item__info']}>
-        <h3 className={styles['recommend-products-item__name']}>
-          {data.title}
-        </h3>
-        <span className={styles['recommend-products-item__price']}>
-          {data.price}
-        </span>
+        <h3 className={styles['recommend-products-item__name']}>{data.name}</h3>
+        {data.saleRatio ? (
+          <span className={styles['recommend-products-item__price']}>
+            <span className={styles['sale-price']}>
+              {data.saleRatio * 100}%
+            </span>
+            {data.price * (1 - data.saleRatio)} 원
+            <div className={styles['origin-price']}>{data.price} 원</div>
+          </span>
+        ) : (
+          <span className={styles['recommend-products-item__price']}>
+            {data.price} 원
+          </span>
+        )}
+
+        {/* {data.saleRatio ? (
+          <span className={styles['recommend-products-item__price']}>
+            {data.price} 원
+          </span>
+        ) : null} */}
       </div>
       <button
         className={styles['recommend-products-item__cartIconBtn']}
