b4269a6fc81be513cdb9fab67fce7cb180ed5d7e
yurim park <yuriming112@gmail.com>
2023-03-17 17:06:16 +0900

    [style] 마크업 및 CSS 스타일링

--- src/components/RegisterRefactor/Btn.jsx
+++ src/components/RegisterRefactor/Btn.jsx
@@ -3,7 +3,7 @@ import styles from './Btn.module.css';
 
 export function Btn({ btnTitle, buttonClassName }) {
   return (
-    <div>
+    <div className={styles['btn-container']}>
       <button type="button" className={styles[buttonClassName]}>
         {btnTitle}
       </button>
--- src/components/RegisterRefactor/UserInput.jsx
+++ src/components/RegisterRefactor/UserInput.jsx
@@ -55,8 +55,8 @@ export function UserInput() {
           <Btn btnTitle="인증번호 받기" buttonClassName="small-btn" />
         </li>
         <li>
-          <label className={styles['register-label-required']}>주소</label>
-          <div>
+          <div className={styles['address-wrapper']}>
+            <label className={styles['register-label-required']}>주소</label>
             <Btn btnTitle="주소 검색" buttonClassName="big-btn" />
             <p className={styles['address-desc']}>
               배송지에 따라 상품 정보가 달라질 수 있습니다.
@@ -94,9 +94,71 @@ export function UserInput() {
         </li>
         <li>
           <label className={styles['register-label']}>생년월일</label>
+          <div className={styles['register-birth-wrapper']}>
+            <label htmlFor="birth-year" className={styles['a11y-hidden']}>
+              연도 입력(4글자)
+            </label>
+            <input
+              type="text"
+              maxLength="4"
+              minLength="4"
+              id="birth-year"
+              name="birth-year"
+              placeholder="YYYY"
+              aria-hidden="true"
+            />
+            <span aria-hidden="true" className={styles['birth-between']}>
+              /
+            </span>
+            <label htmlFor="birth-month" className={styles['a11y-hidden']}>
+              월 입력(2글자)
+            </label>
+            <input
+              type="text"
+              maxLength="2"
+              minLength="2"
+              id="birth-month"
+              name="birth-month"
+              placeholder="MM"
+              aria-hidden="true"
+            />
+            <span aria-hidden="true" className={styles['birth-between']}>
+              /
+            </span>
+            <label htmlFor="birth-day" className={styles['a11y-hidden']}>
+              일 입력(2글자)
+            </label>
+            <input
+              type="text"
+              maxLength="2"
+              minLength="2"
+              id="birth-day"
+              name="birth-day"
+              placeholder="DD"
+              aria-hidden="true"
+            />
+          </div>
         </li>
         <li>
           <label className={styles['register-label']}>추가입력 사항</label>
+          <div className={styles['event-wrapper']}>
+            <input
+              className={styles['a11y-hidden']}
+              type="radio"
+              id="recommand-id"
+              name="extra"
+              value="recommand-id"
+            />
+            <label htmlFor="recommand-id">친구초대 추천인 아이디</label>
+            <input
+              className={styles['a11y-hidden']}
+              type="radio"
+              id="event"
+              name="extra"
+              value="event"
+            />
+            <label htmlFor="event">참여 이벤트명</label>
+          </div>
         </li>
       </ul>
     </div>
