40b487a1d3b0ee7cffe6ac86bb4af3628c0c74d2
somin <smhs12@naver.com>
2023-03-23 23:00:59 +0900

    [feat] localStorage 이용한 코드 전체 recoil로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BaseLayout } from './components/Layout/Layout';
 import './App.module.css';
+import Cart from '@/pages/Cart/Cart';
+import ProductDetail from '@/pages/ProductDetail/ProductDetail';
+import ProductList from '@/pages/ProductList/ProductList';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <BaseLayout>
-          <Routes>
-            {/* <Route path="/home" element={<Home />} />
-            <Route path="/signup" element={<Login />} />
-            <Route path="/signin" element={<Register />} /> */}
-          </Routes>
-        </BaseLayout>
-      </div>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <div className="App">
+          <BaseLayout>
+            <Routes>
+              {/* <Route path="/home" element={<Home />} /> */}
+              {/* <Route path="/signup" element={<Login />} /> */}
+              {/* <Route path="/login" element={<Register />} /> */}
+              {/* 로그인 회원가입 텍스트 스타일 테스트  */}
+              {/* <Route path="/signup" element={<Cart />} /> */}
+              {/* <Route path="/login" element={<Cart />} /> */}
+              <Route path="/product-list" element={<ProductList />} />
+              <Route path="/product-detail" element={<ProductDetail />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </BaseLayout>
+        </div>
+      </Router>
+    </RecoilRoot>
   );
 }
 
