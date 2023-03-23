f08dd2651db760dd5a7199fc2f3a624468013b65
Yurim Park <99720055+yurimpark@users.noreply.github.com>
2023-03-23 23:54:10 +0900

    [feat] 모달창 닫기 기능 추가

--- src/components/Login/LoginModal.jsx
+++ src/components/Login/LoginModal.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../Login/LoginModal.module.css';
 
 export default function LoginModal() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleCloseModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className={styles.modal}>
+    <div className={`${isOpen ? styles.open : styles.closed}`}>
       <div className={styles.modalContent}>
         <div className={styles.modalBody}>
           <h2 className={styles['a11y-hidden']}>로그인 오류</h2>
           <p>아이디, 비밀번호를 확인해주세요.</p>
           <div className={styles['btn-container']}>
-            <button className={styles['modal-close-button']}>확인</button>
+            <button
+              className={styles['modal-close-button']}
+              onClick={handleCloseModal}
+            >
+              확인
+            </button>
           </div>
         </div>
       </div>
