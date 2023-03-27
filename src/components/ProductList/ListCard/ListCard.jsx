237d645d70ecde7ddac372cdd7c43448f97c7a1e
BrightSton <gusehf321@naver.com>
2023-03-27 22:55:03 +0900

    [fix] 모달창이 뜨지 않고 a링크로 이동하던 문제 해결 #150 [feat] List-Card를 누르면 상세 페이지로 이동 #52

--- src/components/ProductList/ListCard/ListCard.jsx
+++ src/components/ProductList/ListCard/ListCard.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react';
-import { CartModal } from '../CartModal/CartModal';
 import styles from './ListCard.module.css';
+
+import { CartModal } from '../CartModal/CartModal';
+import { priceTemplate } from '@/utils/index';
+
+import { useState } from 'react';
 
 export function ListCard({ props }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,19 +15,50 @@ export function ListCard({ props }) {
     setIsModalOpen(false);
   };
 
-  const salePrice = (
-    props.price -
-    props.price * props.saleRatio
-  ).toLocaleString(navigator.language);
-  const originPrice = props.price.toLocaleString(navigator.language);
+  const salePrice = priceTemplate(props.price - props.price * props.saleRatio);
+  const originPrice = priceTemplate(props.price);
 
   return (
-    <a href="#" className={styles['product-each']}>
-      <img
-        className={styles['card-image']}
-        src={props.image.thumbnail}
-        alt={props.image.alt}
-      />
+    <div className={styles['product-each']}>
+      <a href={`/product-detail/${props.id}`}>
+        <img
+          className={styles['card-image']}
+          src={props.image.thumbnail}
+          alt={props.image.alt}
+        />
+        <div className={styles['product-info']}>
+          <span className={styles['product-info__delivery']}>샛별배송</span>
+          <span className={styles['product-info__name']}>{props.name}</span>
+          {props.saleRatio !== 0 ? (
+            <>
+              <div className={styles['price']}>
+                <span className={styles['product-info__sale-ratio']}>
+                  {props.saleRatio * 100}%
+                </span>
+                <span className={styles['product-info__price']}>
+                  {salePrice}원
+                </span>
+              </div>
+              <span className={styles['product-info__origin-price']}>
+                {originPrice}원
+              </span>
+            </>
+          ) : (
+            <span className={styles['product-info__price']}>{salePrice}원</span>
+          )}
+          <span className={styles['product-info__description']}>
+            {props.description}
+          </span>
+          <div className={styles['product-info__badge']}>
+            {props.badge.karly === 'hidden' ? undefined : (
+              <span className={styles['badge-karly']}>Karly Only</span>
+            )}
+            {props.badge.limit === 'hidden' ? undefined : (
+              <span className={styles['badge-limit']}>한정수량</span>
+            )}
+          </div>
+        </div>
+      </a>
       <button
         type="button"
         aria-label="cart-icon"
@@ -32,39 +66,6 @@ export function ListCard({ props }) {
         onClick={openModal}
       ></button>
       {isModalOpen && <CartModal onClose={closeModal} item={props} />}
-
-      <div className={styles['product-info']}>
-        <span className={styles['product-info__delivery']}>샛별배송</span>
-        <span className={styles['product-info__name']}>{props.name}</span>
-        {props.saleRatio !== 0 ? (
-          <>
-            <div className={styles['price']}>
-              <span className={styles['product-info__sale-ratio']}>
-                {props.saleRatio * 100}%
-              </span>
-              <span className={styles['product-info__price']}>
-                {salePrice}원
-              </span>
-            </div>
-            <span className={styles['product-info__origin-price']}>
-              {originPrice}원
-            </span>
-          </>
-        ) : (
-          <span className={styles['product-info__price']}>{salePrice}원</span>
-        )}
-        <span className={styles['product-info__description']}>
-          {props.description}
-        </span>
-        <div className={styles['product-info__badge']}>
-          {props.badge.karly === 'hidden' ? undefined : (
-            <span className={styles['badge-karly']}>Karly Only</span>
-          )}
-          {props.badge.limit === 'hidden' ? undefined : (
-            <span className={styles['badge-limit']}>한정수량</span>
-          )}
-        </div>
-      </div>
-    </a>
+    </div>
   );
 }
